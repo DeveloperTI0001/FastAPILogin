1867c138bb7161a75e5ceeb8dae7cbcc452e5a87
Carlos <martin5435412@gmail.com>
2025-11-14 12:24:05 -0500

    mejorando la seguridad

--- src/db/supabaseServerClient.py
+++ src/db/supabaseServerClient.py
@@ -1,8 +1,12 @@
 from supabase import create_client
 import os
+from dotenv import load_dotenv
 
-supabase_url = os.getenv("SUPABASE_URL") or "https://rnmkcfdwaaeqpbepdstq.supabase.co"
-supabase_service_key = os.getenv("SUPABASE_ROLE_KEY") or "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJubWtjZmR3YWFlcXBiZXBkc3RxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Njg3MTYxMywiZXhwIjoyMDcyNDQ3NjEzfQ.bIBqv0y332Pn_ZZHfAYlv8sYoG0HJL8pwbk1x1WmQFE"
+if os.path.exists(".env"):
+    load_dotenv()
+
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_service_key = os.getenv("SUPABASE_ROLE_KEY") 
 
 if not supabase_url or not supabase_service_key:
     raise Exception("Faltan credenciales de Supabase en el entorno.")
